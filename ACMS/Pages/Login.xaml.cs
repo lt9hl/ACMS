6b52bc1fd3ca49f409ce732688061dee01b94987
chepchili <positivecookie@mail.ru>
2025-03-01 20:30:40 +0300

    reg log

--- ACMS/Pages/Login.xaml.cs
+++ ACMS/Pages/Login.xaml.cs
@@ -1,11 +1,13 @@
 ﻿using ACMS.ApplicationData;
 using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
 using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
@@ -20,69 +22,71 @@ namespace ACMS.Pages
     /// <summary>
     /// Логика взаимодействия для Login.xaml
     /// </summary>
-    public partial class Login : Page
+    public partial class LoginI : Page
     {
-        public Login() {InitializeComponent(); }
+        private void toReg(object sender, RoutedEventArgs e)
+        { 
+            AppFrame.PMain.Navigate(new Reg());
+        }
+            
+
+            public LoginI() {
+                InitializeComponent();
+                LoginButt.IsEnabled = false;
+            }
+
+            private void LoginButt_Click(object sender, RoutedEventArgs e)
+            {
+            try
+            {
+                string logInp = LoginInp.Text;
+                string passInp = PassInp.Password;
+
+                var user = AppConnect.modelOdb.Users.FirstOrDefault(x => x.Password == passInp && logInp == x.Login);
+
+                if (user != null)
+                {
+                    NavigationService.Navigate(new StartPage());
+                }
+                else
+                {
+                    MessageBox.Show("Пользователь не найден", "Ошибка", MessageBoxButton.OK, MessageBoxImage.Information);
+                    LoginInp.Text = "";
+                    PassInp.Password = "";
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Критическая ошибка в работе приложения\n{ex.Message}", "Ошибка", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+
+
+        }
 
 
 
+            private void toRegButt_Click(object sender, RoutedEventArgs e)
+            {
+                NavigationService.Navigate(new Reg());
+            }
+
+            private void PassInp_PasswordChanged(object sender, RoutedEventArgs e)
+            {
+
+                if (PassInp.Password == "")
+                    LoginButt.IsEnabled = false;
+                else
+                    LoginButt.IsEnabled = true;
+            }
 
 
-
-        //{
-        //    InitializeComponent();
-        //    LoginButt.IsEnabled = false;
-
-
-        //}
-
-        //private void LoginButt_Click(object sender, RoutedEventArgs e)
-        //{
-        //    string logInp = LoginInp.Text;
-        //    string passInp = PassInp.Password;
-
-        //    var user = AppConnect.modelOdb.Users.FirstOrDefault(x => x.Password == passInp && logInp == x.Login);
-
-        //    if(user != null)
-        //    {
-        //        NavigationService.Navigate(new StartPage());
-        //    }
-        //    else
-        //    {
-        //        MessageBox.Show("Пользователь не найден", "Ошибка", MessageBoxButton.OK, MessageBoxImage.Information);
-        //        LoginInp.Text = "";
-        //        PassInp.Password = "";
-        //    }
-
-        //}
-
-        //private void toLoginButt_Click(object sender, RoutedEventArgs e)
-        //{
-
-        //}
-
-        //private void toRegButt_Click(object sender, RoutedEventArgs e)
-        //{
-        //    NavigationService.Navigate(new Reg());
-        //}
-
-        //private void PassInp_PasswordChanged(object sender, RoutedEventArgs e)
-        //{
-
-        //    if (PassInp.Password == "")
-        //        LoginButt.IsEnabled = false;
-        //    else
-        //        LoginButt.IsEnabled = true;
-        //}
-
-        //private void LoginButt_MouseEnter(object sender, MouseEventArgs e)
-        //{
-        //    ////DoubleAnimation btnToLog = new DoubleAnimation();
-        //    ////btnToLog.From = 23;
-        //    ////btnToLog.To = 30;
-        //    ////btnToLog.Duration = TimeSpan.FromSeconds(3);
-        //    ////btnToLog.SpeedRatio = 2;
-        //    ////toLoginButt.BeginAnimation(Button.HeightProperty, btnToLog);
-        //}
+        private void LoginInp_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (LoginInp.Text == "")
+                LoginButt.IsEnabled = false;
+            else
+                LoginButt.IsEnabled = true;
+        }
     }
 }
+
--- ACMS/Pages/Reg.xaml.cs
+++ ACMS/Pages/Reg.xaml.cs
@@ -27,12 +27,9 @@ namespace ACMS.Pages
         public Reg()
         {
             InitializeComponent();
+            RegistrDone.IsEnabled = false;
         }
 
-        private void toReg_Click(object sender, RoutedEventArgs e)
-        {
-
-        }
 
         private void toLogin_Click(object sender, RoutedEventArgs e)
         {
@@ -45,27 +42,33 @@ namespace ACMS.Pages
             {
                 string inpPass = Pass.Password;
                 string inpRepPass = RepPass.Password;
-                string inpLogin = Login.Text.Trim();
+                string inpLogin = LoginI.Text.Trim();
                 string inpEmail = Email.Text.Trim();
 
                 var user = AppConnect.modelOdb.Users.FirstOrDefault(x => x.Login == inpLogin);
 
                 Pass.Background = Brushes.Transparent;
                 RepPass.Background = Brushes.Transparent;
-                Login.Background = Brushes.Transparent;
+                LoginI.Background = Brushes.Transparent;
                 Email.Background = Brushes.Transparent;
 
                 if (user != null)
                 {
-                    Login.Background = new SolidColorBrush(Color.FromRgb(99, 32, 36));
+                    LoginI.Background = new SolidColorBrush(Color.FromRgb(202, 63, 40));
                     MessageBox.Show("Имя пользователя занято", "Уведомление", MessageBoxButton.OK, MessageBoxImage.Information);
                 }
                 else if (inpPass.Length < 8 || inpRepPass.Length < 8)
                 {
-                    Pass.Background = new SolidColorBrush(Color.FromRgb(99, 32, 36));
-                    RepPass.Background = new SolidColorBrush(Color.FromRgb(99, 32, 36));
+                    Pass.Background = Brushes.DarkRed;
+                    RepPass.Background = Brushes.DarkRed;
 
                     MessageBox.Show("Длинна пароля меньше 8 символов", "Уведомление", MessageBoxButton.OK, MessageBoxImage.Information);
+                }
+                else if(!inpEmail.Contains("@") || !inpEmail.Contains("."))
+                {
+                    MessageBox.Show("Почта заполнена неправильно", "Уведомление", MessageBoxButton.OK, MessageBoxImage.Information);
+
+                    Email.Background = Brushes.DarkRed;
                 }
                 else
                 {
@@ -95,7 +98,7 @@ namespace ACMS.Pages
            
         void checkPass()
         {
-            if (RepPass.Password != Pass.Password)
+            if (RepPass.Password != Pass.Password || RepPass.Password == null || Pass.Password == null )
                 RegistrDone.IsEnabled = false;
             else
                 RegistrDone.IsEnabled = true;
@@ -106,9 +109,16 @@ namespace ACMS.Pages
             checkPass();
         }
 
-        private void toLogin_MouseEnter(object sender, MouseEventArgs e)
+        private void LoginI_TextChanged(object sender, TextChangedEventArgs e)
         {
+            if (LoginI.Text != null) RegistrDone.IsEnabled = false;
+            else RegistrDone.IsEnabled = true;
+        }
 
+        private void Email_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (Email.Text != null) RegistrDone.IsEnabled = false;
+            else RegistrDone.IsEnabled = true;
         }
     }
 }
